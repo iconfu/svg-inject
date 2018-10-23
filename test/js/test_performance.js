2134f155e09fe001ddd7d7259b034b06b041f692
iconfu <dasboe@me.com>
2018-10-23 10:21:03 +0200

    optimize performance tests
    - add set caching level options
    - add more test SVGs

--- test/js/test_performance.js
+++ test/js/test_performance.js
@@ -1,28 +1,35 @@
 domReady(function(event) {
   var renderContainerElem = document.getElementById('render-container');
   var resultsElem = document.getElementById('results');
+  var build = document.getElementById('build');
+  var disableCacheCheckbox = document.getElementById('cache-disable-svg-inject');
+  var disableBrowserCacheCheckbox = document.getElementById('cache-disable-browser');
 
-  var buildHtml = function(htmlStr) {
-    var div = document.createElement('div');
-    div.innerHTML = htmlStr;
-    return div.firstElementChild;
-  }
-
-  var getImageStr = function(svgUrl, testNum, imageNum) {
-    return '<img src="/imgs/' + svgUrl + '" class="image-' + testNum + '" alt="image ' + testNum + ' ' + imageNum + '">';
+  var createRandomURILString = function(count) {
+    var str = Math.random().toString(36).substring(2, 15);
+    return str + (--count === 0 ? '' : createRandomURILString(count));
   };
 
-  var runPerformanceTest = function(svgUrl, num, sampleSize, insertResultElem, callback) {
+  var buildHtml = function(htmlStr) {
+    build.innerHTML = htmlStr;
+    return build.firstElementChild;
+  };
+
+  var getImageStr = function(svgUrl, testNum, imageNum, disableBrowserCache) {
+    return '<img src="./imgs/' + svgUrl + (disableBrowserCache ? '?imageNum=' + createRandomURILString(4) : '') + '" class="image-' + testNum + '" alt="image ' + testNum + ' ' + imageNum + '">';
+  };
+
+  var runPerformanceTest = function(svgUrl, num, sampleSize, insertResultElem, disableCache, disableBrowserCache, callback) {
     var imgsStr = '';
     for (var i = 0; i < sampleSize; ++i) {
-      imgsStr += getImageStr(svgUrl, num, i);
+      imgsStr += getImageStr(svgUrl, num, i, disableBrowserCache);
     }
 
     renderContainerElem.innerHTML = imgsStr;
 
     if (insertResultElem) {
       var testStr = '<div class="result" id="result-' + num + '">' +
-                      getImageStr(svgUrl, num, 0) +
+                      getImageStr(svgUrl, num, 0, disableBrowserCache) +
                       '<div class="stats"></div>'
                     '</div>';
 
@@ -33,22 +40,20 @@ domReady(function(event) {
 
     var svgInject = SVGInject.create('SVGInject' + num);
 
-    var start;
+    var start = new Date().getTime();
 
     svgInject(document.querySelectorAll('img.image-' + num), {
-      afterLoad: function() {
-        start = new Date().getTime();
-      },
       onAllFinish: function() {
         var time = new Date().getTime() - start;
 
         renderContainerElem.innerHTML = '';
         callback(testElem, time);
-      }
+      },
+      useCache: !disableCache
     });
   };
 
-  var runPerformanceTests = function(svgUrls, count, sampleSize, repetitions, callback) {
+  var runPerformanceTests = function(svgUrls, count, sampleSize, repetitions, disableCache, disableBrowserCache, callback) {
     if (svgUrls.length == count) {
       callback();
     } else {
@@ -57,15 +62,15 @@ domReady(function(event) {
       var repetitionCount = repetitions;
 
       var runTest = function() {
-        runPerformanceTest(svgUrl, count, sampleSize, repetitionCount == 1, function(testElem, time) {
+        runPerformanceTest(svgUrl, count, sampleSize, repetitionCount == 1, disableCache, disableBrowserCache, function(testElem, time) {
           bestTime = bestTime && bestTime < time ? bestTime : time;
           
           if (--repetitionCount == 0) {
-            var statsStr = '<div class="stats">' + bestTime / (sampleSize) + 'ms</div>';
+            var statsStr = '<div class="stats">' + bestTime / sampleSize + 'ms</div>';
               
             testElem.getElementsByClassName('stats')[0].innerHTML = statsStr;
 
-            runPerformanceTests(svgUrls, ++count, sampleSize, repetitions, callback);
+            runPerformanceTests(svgUrls, ++count, sampleSize, repetitions, disableCache, disableBrowserCache, callback);
           } else {
             runTest();
           }
@@ -83,8 +88,15 @@ domReady(function(event) {
 
     var sampleSize = parseInt(document.getElementById('sample-size').value);
     var repetitions = parseInt(document.getElementById('repetitions').value);
+    var disableCache = disableCacheCheckbox.checked;
+    var disableBrowserCache = disableBrowserCacheCheckbox.checked;
 
-    runPerformanceTests(['test1.svg', 'performance-1.svg'], 0, sampleSize, repetitions, function() {
+    var svgUrls = [];
+    for (var i = 1; i <= 10; ++i) {
+      svgUrls.push('performance/' + i + '.svg');
+    }
+
+    runPerformanceTests(svgUrls, 0, sampleSize, repetitions, disableCache, disableBrowserCache, function() {
       document.body.className = '';
     });
   };  
