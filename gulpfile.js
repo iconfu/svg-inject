e23c27a375942753b905fda87c22bb92670f7b82
iconfu <dasboe@me.com>
2018-10-23 16:27:39 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const pump = require('pump');
 
-gulp.task('default', function (cb) {
+gulp.task('svg-inject', function (cb) {
   pump([
     gulp.src('src/svg-inject.js'),
     gulp.dest('dist/'),
@@ -23,9 +23,15 @@ gulp.task('default', function (cb) {
     rename({ extname: '.min.js' }),
     gulp.dest('examples/'),
     gulp.dest('test/js/'),
-    gulp.dest('dist/'),
+    gulp.dest('dist/')
+  ], cb);
+});
 
+gulp.task('xhr-check', function (cb) {
+  pump([
     gulp.src('test/js/xhr-check.js'),
     gulp.dest('examples/_example_helpers/')
   ], cb);
 });
+
+gulp.task('default', ['svg-inject', 'xhr-check']);
