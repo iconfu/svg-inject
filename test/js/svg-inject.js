496201c7a34a516cf57f46a63012af9f82daed70
iconfu <dasboe@me.com>
2018-10-24 18:43:27 +0200

    Version 1.2.0

--- test/js/svg-inject.js
+++ test/js/svg-inject.js
@@ -1,5 +1,5 @@
 /**
- * SVGInject - Version 1.1.2
+ * SVGInject - Version 1.2.0
  * A tiny, intuitive, robust, caching solution for injecting SVG files inline into the DOM.
  *
  * https://github.com/iconfu/svg-inject
