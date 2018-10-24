f970248051b779e0eaf1fb3579fd54ff83c7139f
iconfu <dasboe@me.com>
2018-10-24 13:45:13 +0200

    checkbox for using onload attribute in performance tests

--- test/js/test_performance.js
+++ test/js/test_performance.js
@@ -2,8 +2,9 @@ domReady(function(event) {
   var renderContainerElem = document.getElementById('render-container');
   var resultsElem = document.getElementById('results');
   var build = document.getElementById('build');
-  var disableCacheCheckbox = document.getElementById('cache-disable-svg-inject');
-  var disableBrowserCacheCheckbox = document.getElementById('cache-disable-browser');
+  var disableCacheRadio = document.getElementById('cache-disable-svg-inject');
+  var disableBrowserCacheRadio = document.getElementById('cache-disable-browser');
+  var useOnloadAttributeCheckbox = document.getElementById('use-onload-attribute');
 
   var createRandomURILString = function(count) {
     var str = Math.random().toString(36).substring(2, 15);
@@ -15,21 +16,45 @@ domReady(function(event) {
     return build.firstElementChild;
   };
 
-  var getImageStr = function(svgUrl, testNum, imageNum, disableBrowserCache) {
-    return '<img src="./imgs/' + svgUrl + (disableBrowserCache ? '?imageNum=' + createRandomURILString(4) : '') + '" class="image-' + testNum + '" alt="image ' + testNum + ' ' + imageNum + '">';
+  var getImageStr = function(svgUrl, testNum, imageNum, disableBrowserCache, useOnload) {
+    return '<img src="./imgs/' + svgUrl + (disableBrowserCache ? '?imageNum=' + createRandomURILString(4) : '') + '"' + (useOnload ? ' onload="SVGInject' + testNum + '(this)"' : '') + ' class="image-' + testNum + '" alt="image ' + testNum + ' ' + imageNum + '">';
   };
 
-  var runPerformanceTest = function(svgUrl, num, sampleSize, insertResultElem, disableCache, disableBrowserCache, callback) {
+  var runPerformanceTest = function(svgUrl, num, sampleSize, insertResultElem, disableCache, disableBrowserCache, useOnload, callback) {
     var imgsStr = '';
     for (var i = 0; i < sampleSize; ++i) {
-      imgsStr += getImageStr(svgUrl, num, i, disableBrowserCache);
+      imgsStr += getImageStr(svgUrl, num, i, disableBrowserCache, useOnload);
     }
 
     renderContainerElem.innerHTML = imgsStr;
 
+    var svgInject = SVGInject.create('SVGInject' + num);
+
+    var afterAllInjected = function(callback) {
+      window.requestAnimationFrame(function() {
+        var time = new Date().getTime() - startTime;
+
+        window.requestAnimationFrame(function() {
+          callback(testElem, time);
+        });
+      });
+    };
+
+    if (useOnload) {
+      var injectCount = 0;
+      svgInject.setOptions({
+        afterInject: function() {
+          if (++injectCount == sampleSize) {
+            afterAllInjected(callback);
+          }
+        }
+      });
+    }
+    
+
     if (insertResultElem) {
       var testStr = '<div class="result" id="result-' + num + '">' +
-                      getImageStr(svgUrl, num, 0, disableBrowserCache) +
+                      getImageStr(svgUrl, num, 0, disableBrowserCache, useOnload) +
                       '<div class="stats"></div>'
                     '</div>';
 
@@ -38,27 +63,21 @@ domReady(function(event) {
       resultsElem.appendChild(testElem);
     }
 
-    var svgInject = SVGInject.create('SVGInject' + num);
+    var startTime = new Date().getTime();
 
-    var start = new Date().getTime();
-
-    svgInject(document.querySelectorAll('img.image-' + num), {
-      onAllFinish: function() {
-        window.requestAnimationFrame(function() {
-          var time = new Date().getTime() - start;
-
-          renderContainerElem.innerHTML = '';
-
+    if (!useOnload) {
+      svgInject(document.querySelectorAll('img.image-' + num), {
+        onAllFinish: function() {
           window.requestAnimationFrame(function() {
-            callback(testElem, time);
+            afterAllInjected(callback);
           });
-        });
-      },
-      useCache: !disableCache
-    });
+        },
+        useCache: !disableCache
+      });
+    }
   };
 
-  var runPerformanceTests = function(svgUrls, count, sampleSize, repetitions, disableCache, disableBrowserCache, callback) {
+  var runPerformanceTests = function(svgUrls, count, sampleSize, repetitions, disableCache, disableBrowserCache, useOnload, callback) {
     if (svgUrls.length == count) {
       callback();
     } else {
@@ -67,7 +86,7 @@ domReady(function(event) {
       var repetitionCount = repetitions;
 
       var runTest = function() {
-        runPerformanceTest(svgUrl, count, sampleSize, repetitionCount == 1, disableCache, disableBrowserCache, function(testElem, time) {
+        runPerformanceTest(svgUrl, count, sampleSize, repetitionCount == 1, disableCache, disableBrowserCache, useOnload, function(testElem, time) {
           bestTime = bestTime && bestTime < time ? bestTime : time;
           
           if (--repetitionCount == 0) {
@@ -75,7 +94,7 @@ domReady(function(event) {
               
             testElem.getElementsByClassName('stats')[0].innerHTML = statsStr;
 
-            runPerformanceTests(svgUrls, ++count, sampleSize, repetitions, disableCache, disableBrowserCache, callback);
+            runPerformanceTests(svgUrls, ++count, sampleSize, repetitions, disableCache, disableBrowserCache, useOnload, callback);
           } else {
             runTest();
           }
@@ -93,15 +112,16 @@ domReady(function(event) {
 
     var sampleSize = parseInt(document.getElementById('sample-size').value);
     var repetitions = parseInt(document.getElementById('repetitions').value);
-    var disableCache = disableCacheCheckbox.checked;
-    var disableBrowserCache = disableBrowserCacheCheckbox.checked;
+    var disableCache = disableCacheRadio.checked;
+    var disableBrowserCache = disableBrowserCacheRadio.checked;
+    var useOnload = useOnloadAttributeCheckbox.checked;
 
     var svgUrls = [];
     for (var i = 1; i <= 10; ++i) {
       svgUrls.push('performance/' + i + '.svg');
     }
 
-    runPerformanceTests(svgUrls, 0, sampleSize, repetitions, disableCache, disableBrowserCache, function() {
+    runPerformanceTests(svgUrls, 0, sampleSize, repetitions, disableCache, disableBrowserCache, useOnload, function() {
       document.body.className = '';
     });
   };  
